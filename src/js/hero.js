b7a1e48613dbed52b1948d9b7a6662ff1551e06e
Leonid <zemlyanykh@bk.ru>
2024-10-25 16:31:53 +0500

    Выполнение задания

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,7 +1,17 @@
+const characters = [
+  { name: 'мечник', health: 10, expected: 'critical' },
+  { name: 'маг', health: 100, expected: 'healthy' },
+  { name: 'лучник', health: 80, expected: 'healthy' },
+  { name: 'воин', health: 45, expected: 'wounded' },
+];
+
+export const sortedCharacters = characters.sort((a, b) => b.health - a.health);
+
 export default function Character(characteristics) {
   if (characteristics.health < 15) {
     return 'critical';
-  } if (characteristics.health > 15 && characteristics.health < 50) {
+  }
+  if (characteristics.health > 15 && characteristics.health < 50) {
     return 'wounded';
   }
   return 'healthy';
--- src/tests/hero.test.js
+++ src/tests/hero.test.js
@@ -1,30 +1,6 @@
-import Character from '../js/hero';
+import Character, { sortedCharacters } from '../js/hero';
 
-test('Health hero is equal healthy', () => {
-  const result = Character({ name: 'Маг', health: 90 });
-
-  expect(result).toBe('healthy');
-});
-
-test.each(
-  [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-  ]
-)('testing heros health with %s status and %i health', ({ name, health, expected }) => {
+test.each(sortedCharacters)('testing heros health with %s status and %i health', ({ name, health, expected }) => {
   const result = Character({ name, health });
   expect(result).toBe(expected);
-});
-
-test('Health hero is equal wounded', () => {
-  const result = Character({ name: 'Воин', health: 45 });
-
-  expect(result).toBe('wounded');
-});
-
-test('Health hero is equal critical', () => {
-  const result = Character({ name: 'Замок', health: 10 });
-
-  expect(result).toBe('critical');
 });
